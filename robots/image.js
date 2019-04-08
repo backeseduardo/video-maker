d426dafa9469adbc2de07ae16a62b147b7b12fe1
Eduardo Backes <e.backes2011@gmail.com>
2019-04-08 16:17:08 -0300

    feat: add image download logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state')
@@ -8,6 +9,7 @@ async function robot() {
   const content = state.load()
 
   await fetchImagesOfAllSentences(content)
+  await downloadAllImages(content)
 
   state.save(content)
 
@@ -32,6 +34,37 @@ async function robot() {
     const imagesUrl = response.data.items.map(item => item.link)
 
     return imagesUrl
+  }
+
+  async function downloadAllImages(content) {
+    content.downloadedImages = []
+
+    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+      const images = content.sentences[sentenceIndex].images
+
+      for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+        const imageUrl = images[imageIndex]
+
+        try {
+          if (content.downloadedImages.includes(imageUrl)) {
+            throw new Error('Imagem já foi baixada')
+          }
+          await downloadAndSave(imageUrl, `${sentenceIndex}--original.png`)
+          content.downloadedImages.push(imageUrl)
+          console.log(`> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`)
+          break
+        } catch (error) {
+          console.error(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
+        }
+      }
+    }
+  }
+
+  async function downloadAndSave(url, fileName) {
+    return imageDownloader.image({
+      url: url,
+      dest: `./content/${fileName}`
+    })
   }
 
 }
